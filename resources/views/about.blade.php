f4d3ed6f3db4941f287609984f72ac411529d113
Ielfran <gaganraiqpwo@gmail.com>
2024-06-20 22:30:39 +0545

    new first

---
+++ resources/views/about.blade.php
@@ -0,0 +1,40 @@
+@extends('components.masterlayout')
+
+@section('title', 'About Us')
+
+@section('content')
+<div class="lg:h-screen">
+    <div class="bg-cover bg-center h-96" style="background-image: url('/images/aboutUs.webp');">
+        <p class="font-bold">sdhvsuibviusdvbuisdvsdv</p>
+    </div>
+
+    <div class="container mx-auto px-6 py-8 text-lg w-4/5 sm:w-3/4 lg:w-1/2">
+        <section class=" bg-white bg-opacity-80 shadow-lg rounded-lg p-8 mt-10">
+            <h1 class="text-4xl font-bold mb-6 text-center">About Us</h1>
+            <p class="mb-4 text-gray-700 leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+            </p>
+            <p class="mb-4 text-gray-700 leading-relaxed">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+            </p>
+            <p class="text-gray-700 leading-relaxed">
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                magni dolores eos qui ratione voluptatem sequi nesciunt.
+            </p>
+            <p>sdnviosdhviosdvvsd</p>
+            <p>
+                svnuisdovvsdv
+            </p>
+            <p>
+                sdgvsdbsdbmoi
+                dsvdvvdggsegsgeg
+        </p>
+        </section>
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,7 @@
+@extends    ('components.masterlayout')
+
+@section('title', 'Home')
+
+@section('content')
+
+@endsection
